56c21690ccb5462f0690d8fca12f7d1bad817368
TheMadPig <49966112+TheMadPig@users.noreply.github.com>
2020-05-02 14:52:26 -0400

    Exercise 1.0 and 1.1

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -9,3 +9,29 @@
 // OPTIONAL
 // Feel free to add some CSS to this once you're done
 // --------------------------------------------------
+
+// select body
+let body = document.querySelector('body');
+// initiate counter
+let counter = 0;
+
+body.addEventListener('click', function() {
+  // increment counter
+  counter++;
+  // create div element
+  let div = document.createElement('div');
+  // insert div in body
+  body.appendChild(div);
+  // change background-color depending on odd or even row
+  if (counter % 2 === 0) {
+    // background color orange
+    div.style.backgroundColor = '#ff7c50';
+    // change innerText
+    div.innerText = 'clack';
+  } else {
+    // background color blue
+    div.style.backgroundColor = '#57f2ff';
+    // change innerText
+    div.innerText = 'click';
+  }
+})
--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -13,3 +13,60 @@
 // OPTIONAL
 // Feel free to add some CSS to this once you're done
 // --------------------------------------------------
+
+// select the body
+let body = document.querySelector('body');
+// select the button to start the game
+let button = document.querySelector('Button');
+// select the paragraph to show the results
+let resultHTML = document.querySelector('p');
+// variable for the delay function call
+let time;
+
+button.addEventListener('click', function(event) {
+  // update background to not be red or green
+  resultHTML.style.backgroundColor = '#fff';
+  // update message to be neutral
+  resultHTML.innerText = 'You have 1 second to click in the white area';
+  // change result color to make it visible
+  resultHTML.style.color = '#000';
+  // change button message
+  button.innerText = 'Click, Quick!';
+  // stop the click from the button to bubble up to the body
+  event.stopPropagation();
+  // call the start game function
+  startGame();
+})
+
+function gameWin() {
+  // change result background to green
+  resultHTML.style.backgroundColor = '#31ce31';
+  // change result text color to white 
+  resultHTML.style.color = '#fff';
+  // change result message
+  resultHTML.innerText = 'Success';
+  // change button message
+  button.innerText = 'Good job, Try again';
+  // clear the timeout to cancel the red screen to appear
+  clearTimeout(time);
+}
+
+function gameLoss() {
+  // change result background to red
+  resultHTML.style.backgroundColor = '#ce3131';
+  // change result text color to white 
+  resultHTML.style.color = '#fff';  
+  // change result message
+  resultHTML.innerText = 'Better luck next time';  
+  // change button message
+  button.innerText = 'Try again';  
+  // remove the event so the function gameWin doesn't work
+  body.removeEventListener('click', gameWin);
+}
+
+function startGame() {
+  // setTimeout so that after 1second game is lost
+  time = setTimeout(gameLoss, 1000);
+  // click event listener to win game
+  body.addEventListener('click', gameWin)
+}
